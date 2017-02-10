c361148d6cee3aa722f2b7c48b8d8a0a8e57bbf1
Oleg <ov_nj@outlook.com>
2017-02-10 14:17:43 +0200

    Update args0.java

--- args0.java
+++ args0.java
@@ -4,9 +4,11 @@ import java.io.*;
 class args0 {
   public static void main (String[] args) throws FileNotFoundException {
     Scanner sc = new Scanner((new File(args[0])));
+    while (sc.hasNext()) {
     String arg = sc.next();
-    sc.close();
     System.out.println (arg);
+    }
+    sc.close();
   }
 }
 
