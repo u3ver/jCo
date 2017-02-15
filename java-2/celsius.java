7ac33243de3743f713b19d6983d8003868b7a3cd
Oleg <ov_nj@outlook.com>
2017-02-15 14:28:58 +0200

    Update celsius.java

--- java-2/celsius.java
+++ java-2/celsius.java
@@ -7,8 +7,11 @@ class celsius {
      double c = sc.nextDouble();
      double f;
      f = c * 1.8 + 32;
-     BigDecimal fDec = new BigDecimal(f); // call the rounding 
-     fDec = fDec.setScale(2, BigDecimal.ROUND_HALF_UP); // setting scale to 2numbers after dot + necessary rounding method!!
-  System.out.println ("temperature aboard: " + c + "C.\nin Fahrenheit: " + fDec + "F");
+     f = f * 100;
+     f = (double)((int)f);
+     f = f / 100;
+     //BigDecimal fDec = new BigDecimal(f); // call the rounding 
+     //fDec = fDec.setScale(2, BigDecimal.ROUND_HALF_UP); // setting scale to 2numbers after dot + necessary rounding method!!
+  System.out.println ("temperature aboard: " + c + "C.\nin Fahrenheit: " + f + "F");
   }
 }
