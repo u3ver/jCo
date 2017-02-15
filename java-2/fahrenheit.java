8323ba21bb44a19466f395fae0c14cac1b38ba39
Oleg <ov_nj@outlook.com>
2017-02-15 14:20:26 +0200

    Update fahrenheit.java

--- java-2/fahrenheit.java
+++ java-2/fahrenheit.java
@@ -7,8 +7,11 @@ class fahrenheit {
      double f = sc.nextDouble();
      double c;
        c =  (f - 32) * 0.5556; // 5/9 = 0.555555556
-          BigDecimal cDec = new BigDecimal(c);
-          cDec = cDec.setScale(2, BigDecimal.ROUND_HALF_UP);
-  System.out.println ("Temperature set to: " + f + " F.\n" + "Celsius: " + cDec + " C.");
+    c = c * 100;
+    c = (double)((int) c);
+    c = c / 100;
+         // BigDecimal cDec = new BigDecimal(c);
+         // cDec = cDec.setScale(2, BigDecimal.ROUND_HALF_UP);
+  System.out.println ("Temperature set to: " + f + " F.\n" + "Celsius: " + c + " C."); // + cDec
   }
 }
