7053ce5e037e7c8db1a856db63600a6163c6de16
Oleg <ov_nj@outlook.com>
2017-02-16 15:32:06 +0200

    Create Evenly.java

---
+++ java3/Evenly.java
@@ -0,0 +1,18 @@
+// if either evenly divides the other
+
+import java.util.Scanner;
+
+class Evenly {
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(args[0]);
+      int a = sc.nextInt();
+    sc.close();
+    Scanner sc2 = new Scanner(args[1]);
+      int b = sc2.nextInt();
+    sc2.close();
+       for (
+       
+    System.out.println ("non int char");
+     }
+  }
+}
