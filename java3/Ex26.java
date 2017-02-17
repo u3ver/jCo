c427ef14fdd68809c5dc0e26907da02358f7db7e
Oleg <ov_nj@outlook.com>
2017-02-17 10:00:07 +0200

    Update and rename Ex26 !!!! to Ex26.java

--- java3/Ex26.java
+++ java3/Ex26.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
 
-class Test {
+class Ex26 {
   public static void main (String[] args) {
     System.out.println ("enter the time in seconds: ");
     Scanner sc = new Scanner(System.in);
       int N = sc.nextInt();
     sc.close();
     
-      int min = N / 60;
       int hour = N / 3600;
-      int sec = N / min;
-    
-      System.out.println ( hour + "h. " + min + "m. " + sec + "sec.");
+      int min = (N % 3600) / 60;
+      int sec = N % 60;
+      
+        System.out.println ( hour + "h. " + min + "m. " + sec + "sec.");
   }
 }
