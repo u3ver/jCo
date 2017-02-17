1f423ce3c09c3a582c14d1f24502a12ab4078e97
Oleg <ov_nj@outlook.com>
2017-02-17 13:59:42 +0200

    Update Ex29.java

--- java3/Ex29.java
+++ java3/Ex29.java
@@ -4,18 +4,18 @@ class Test {
   public static void main (String[] args) {
     System.out.println ("enter digit from 1000-9999: ");
     Scanner sc = new Scanner(System.in);
-      int xxx = sc.nextInt();
+      int x = sc.nextInt();
     sc.close();
     
-    int thou = xxx / 1000;
-    int hund = (xxx / 100) % 10;
-    int dec = (xxx / 10) % 10;
-    int unit = (xxx % 10);
+    int thou = x / 1000;
+    int hund = (x / 100) % 10;
+    int dec = (x / 10) % 10;
+    int unit = x % 10;
     
-    System.out.println (thou);
-    System.out.println (hund);
-    System.out.println (dec);
-    System.out.println (unit);
+    //System.out.println (thou);
+    //System.out.println (hund);
+    //System.out.println (dec);
+    //System.out.println (unit);
     
     boolean q;
     if ( q = (thou + hund == dec + unit)) {
